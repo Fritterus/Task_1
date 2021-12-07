d0813bfc59630ff190fb555984c29da23813ce43
Fritterus <62291430+Fritterus@users.noreply.github.com>
2021-12-07 14:28:40 +0300

    Added ServiceMapper.

---
+++ BLL/Mappers/ServiceMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BLL.ModelsDTO;
+using DAL.Models;
+
+namespace BLL.Mappers
+{
+    internal class ServiceMapperProfile : Profile
+    {
+        public ServiceMapperProfile()
+        {
+            CreateMap<UserDTO, User>();
+            CreateMap<User, UserDTO>();
+        }
+
+    }
+}
