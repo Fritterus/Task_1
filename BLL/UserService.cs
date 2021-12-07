ce8c66c4fb51d5365d062afdb3b88f7446eae2b6
Fritterus <62291430+Fritterus@users.noreply.github.com>
2021-12-07 14:34:12 +0300

    Added DI. Added minor changes.

--- BLL/UserService.cs
+++ BLL/UserService.cs
@@ -1,26 +1,34 @@
-﻿using DAL.Interfaces;
+﻿using AutoMapper;
+using BLL.Interfaces;
+using BLL.ModelsDTO;
+using DAL.Interfaces;
 using DAL.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace BLL
 {
     internal class UserService : IUserService
     {
         private readonly IRepository<User> _db;
+        private readonly IMapper _mapper;
 
-        public UserService(IRepository<User> db)
+        public UserService(IRepository<User> db, IMapper mapper)
         {
             _db = db;
+            _mapper = mapper;
         }
 
         /// <inheritdoc/>
         public int Count => _db.GetAll().Count();
 
         /// <inheritdoc/>
-        public List<User> GetAllUsers(int from, int count)
+        public List<UserDTO> GetAllUsers(int from, int count)
         {
-            return _db.GetAll().Skip(from).Take(count).ToList();
+            var users = _db.GetAll().Skip(from).Take(count).ToList();
+            return _mapper.Map<List<UserDTO>>(users);
         }
     }
 }
