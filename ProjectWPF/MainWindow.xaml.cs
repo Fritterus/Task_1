beaa05382344769cca31b85db1da5ced0c02bca5
Fritterus <62291430+Fritterus@users.noreply.github.com>
2021-12-07 14:35:22 +0300

    Add new Classes. Rewritten xaml-markup. UI logic is moved to other class.

--- ProjectWPF/MainWindow.xaml.cs
+++ ProjectWPF/MainWindow.xaml.cs
@@ -1,10 +1,11 @@
-﻿using BLL;
-using BLL.Interfaces;
+﻿using BLL.Interfaces;
 using DAL.Models;
 using Microsoft.Win32;
+using ProjectWPF.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace ProjectWPF
@@ -14,140 +15,10 @@ namespace ProjectWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly IFileService _fileService;
-        private readonly IFilter _filter;
-        private readonly IUserService _userService;
-
-        private int _currentPage = 0;
-        private const int RECORDS = 10;
-
         public MainWindow(IFileService fileService, IFilter filter, IUserService userService)
         {
             InitializeComponent();
-            _fileService = fileService;
-            _filter = filter;
-            _userService = userService;
-
-            UsersGrid.ItemsSource = _userService.GetAllUsers(0, RECORDS);
-        }
-
-
-        private void Prev_Click(object sender, RoutedEventArgs e)
-        {
-            if (_currentPage == 0)
-            {
-                return;
-            }
-
-            UsersGrid.ItemsSource = _userService.GetAllUsers((--_currentPage) * RECORDS, RECORDS);
-        }
-
-        private void Next_Click(object sender, RoutedEventArgs e)
-        {
-            if (RECORDS > _userService.Count)
-            {
-                return;
-            }
-
-            if (_currentPage >= (_userService.Count / RECORDS))
-            {
-                return;
-            }
-
-            UsersGrid.ItemsSource = _userService.GetAllUsers((++_currentPage) * RECORDS, RECORDS);
-        }
-
-        private async void BtnOpenFile_Click(object sender, RoutedEventArgs e)
-        {
-
-            var openFileDialog = new OpenFileDialog
-            {
-                Filter = "Csv files (*.csv)|*.csv"
-            };
-
-            if (openFileDialog.ShowDialog() == true)
-            {
-                var pathFile = openFileDialog.FileName;
-
-                try
-                {
-                    await _fileService.ImportFromCsvToDatabase(pathFile);
-                }
-                catch (IOException ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-
-                UsersGrid.ItemsSource = _userService.GetAllUsers(_currentPage, RECORDS);
-            }
-        }
-
-        private void BtnSaveToFile_Click(object sender, RoutedEventArgs e)
-        {
-            string name = Name.Text;
-            string lastName = LastName.Text;
-            string patronymic = Patronymic.Text;
-            string city = City.Text;
-            string country = Country.Text;
-
-            DateTime? date = null;
-
-            try
-            {
-                if (!string.IsNullOrEmpty(Date.Text))
-                {
-                    date = DateTime.ParseExact(Date.Text, "dd.MM.yyyy", null);
-                }
-            }
-            catch (FormatException ex)
-            {
-                MessageBox.Show(ex.Message);
-                return;
-            }
-
-            var users = new List<User>();
-
-            try
-            {
-                users = _filter.Filtration(date, name, lastName, patronymic, city, country);
-            }
-            catch(ArgumentException ex)
-            {
-                MessageBox.Show(ex.Message);
-                return;
-            }
-
-            var saveFileDialog = new SaveFileDialog
-            {
-                Filter = "Csv file (*.csv)|*.csv|Xml file (*.xml)|*.xml"
-            };
-
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                var pathFile = saveFileDialog.FileName;
-                var fileFormat = Path.GetExtension(saveFileDialog.FileName);
-
-                try
-                {
-                    if (fileFormat == ".csv")
-                    {
-                        _fileService.WriteToCsvFile(pathFile, users);
-                    }
-                    else if (fileFormat == ".xml")
-                    {
-                        _fileService.WriteToXmlFile(pathFile, users);
-                    }
-                    else
-                    {
-                        throw new IOException("Wrong file format.");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                    return;
-                }
-            }
+            DataContext = new MainViewModel(fileService, filter, userService);
         }
     }
 }
---
+++ ProjectWPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,156 @@
+﻿using BLL.Interfaces;
+using BLL.ModelsDTO;
+using Microsoft.Win32;
+using ProjectWPF.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace ProjectWPF.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly IFileService _fileService;
+        private readonly IFilter _filter;
+        private readonly IUserService _userService;
+
+        private int _currentPage = 0;
+        private const int RECORDS = 10;
+
+        private ObservableCollection<UserDTO> _users;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Patronymic { get; set; } = "";
+        public string City { get; set; } = "";
+        public string Country { get; set; } = "";
+        public DateTime? Date { get; set; } = null;
+
+        public ObservableCollection<UserDTO> Users
+        {
+            get { return _users; }
+            set
+            {
+                _users = value;
+                OnPropertyChanged("Users");
+            }
+        }
+
+        public MainViewModel(IFileService fileService, IFilter filter, IUserService userService)
+        {
+            _fileService = fileService;
+            _filter = filter;
+            _userService = userService;
+
+            Users = new ObservableCollection<UserDTO>(_userService.GetAllUsers(0, RECORDS));
+        }
+
+        public RelayCommand ShowPreviousPage => new(obj =>
+        {
+            if (_currentPage == 0)
+            {
+                return;
+            }
+
+            Users = new ObservableCollection<UserDTO>(_userService.GetAllUsers((--_currentPage) * RECORDS, RECORDS));
+        });
+
+        public RelayCommand ShowNextPage => new(obj =>
+        {
+            if (RECORDS > _userService.Count)
+            {
+                return;
+            }
+
+            if (_currentPage >= (_userService.Count / RECORDS))
+            {
+                return;
+            }
+
+            Users = new ObservableCollection<UserDTO>(_userService.GetAllUsers((++_currentPage) * RECORDS, RECORDS));
+        });
+
+        public RelayCommand OpenFile => new(obj =>
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Csv files (*.csv)|*.csv"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var pathFile = openFileDialog.FileName;
+
+                try
+                {
+                    _fileService.ImportFromCsvToDatabase(pathFile);
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                Users = new ObservableCollection<UserDTO>(_userService.GetAllUsers(_currentPage, RECORDS));
+            }
+        });
+
+        public RelayCommand SaveToFile => new(obj =>
+        {
+            var users = new List<UserDTO>();
+
+            try
+            {
+                users = _filter.Filtration(Date, Name, LastName, Patronymic, City, Country);
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Csv file (*.csv)|*.csv|Xml file (*.xml)|*.xml"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                var pathFile = saveFileDialog.FileName;
+                var fileFormat = Path.GetExtension(saveFileDialog.FileName);
+
+                try
+                {
+                    if (fileFormat == ".csv")
+                    {
+                        _fileService.WriteToCsvFile(pathFile, users);
+                    }
+                    else if (fileFormat == ".xml")
+                    {
+                        _fileService.WriteToXmlFile(pathFile, users);
+                    }
+                    else
+                    {
+                        throw new IOException("Wrong file format.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+            }
+        });
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
